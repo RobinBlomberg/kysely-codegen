24a10032c38fe5c3861cbe67a04d6a12159f2d4b
harisraharjo <haris.r.putro@gmail.com>
2023-02-01 22:11:40 +0700

    remove test for prepublishOnly

--- src/dialects/planetscale/planetscale-dialect.ts
+++ src/dialects/planetscale/planetscale-dialect.ts
@@ -1,13 +1,14 @@
 import { CreateKyselyDialectOptions, Dialect } from '../../dialect';
 import { MysqlAdapter } from '../mysql/mysql-adapter';
 import { MysqlIntrospector } from '../mysql/mysql-introspector';
-import { PlanetScaleDialect } from './pscale-dialect';
 
 export class PlanetscaleDialect extends Dialect {
   readonly adapter = new MysqlAdapter();
   readonly introspector = new MysqlIntrospector();
 
   async createKyselyDialect(options: CreateKyselyDialectOptions) {
+    const { PlanetScaleDialect } = await import('./pscale-dialect');
+
     return new PlanetScaleDialect({ url: options.connectionString });
   }
 }
