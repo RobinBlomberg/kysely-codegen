76d5c8efdf009f8918416d0f76c3012528801ce1
harisraharjo <haris.r.putro@gmail.com>
2023-02-01 23:42:47 +0700

    bundle the cli as well

--- src/dialects/planetscale/planetscale-dialect.ts
+++ src/dialects/planetscale/planetscale-dialect.ts
@@ -1,13 +1,13 @@
 import { CreateKyselyDialectOptions, Dialect } from '../../dialect';
 import { MysqlAdapter } from '../mysql/mysql-adapter';
 import { MysqlIntrospector } from '../mysql/mysql-introspector';
-import { PlanetScaleDialect } from './pscale-dialect';
+import { PlanetScaleDialect as PD } from './pscale-dialect';
 
 export class PlanetscaleDialect extends Dialect {
   readonly adapter = new MysqlAdapter();
   readonly introspector = new MysqlIntrospector();
 
   async createKyselyDialect(options: CreateKyselyDialectOptions) {
-    return new PlanetScaleDialect({ url: options.connectionString });
+    return new PD({ url: options.connectionString });
   }
 }
