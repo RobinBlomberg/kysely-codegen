dbdc30c12f0b79a8705f10bd67ed68bfd9dc7a08
Robin Blomberg <hello@robinblomberg.se>
2024-11-05 07:57:18 +0100

    refactor(introspector): export database metadata options

--- src/introspector/metadata/database-metadata.ts
+++ src/introspector/metadata/database-metadata.ts
@@ -2,17 +2,16 @@ import { EnumCollection } from '../enum-collection';
 import type { TableMetadataOptions } from './table-metadata';
 import { TableMetadata } from './table-metadata';
 
+export type DatabaseMetadataOptions = {
+  enums?: EnumCollection;
+  tables: TableMetadataOptions[];
+};
+
 export class DatabaseMetadata {
   readonly enums: EnumCollection;
   readonly tables: TableMetadata[];
 
-  constructor({
-    enums,
-    tables,
-  }: {
-    enums?: EnumCollection;
-    tables: TableMetadataOptions[];
-  }) {
+  constructor({ enums, tables }: DatabaseMetadataOptions) {
     this.enums = enums ?? new EnumCollection();
     this.tables = tables.map((table) => new TableMetadata(table));
   }
